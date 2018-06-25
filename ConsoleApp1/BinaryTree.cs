295f181e05f73592dbec44d738f22c7e6c2739a1
John Campbell <buyjohn@gmail.com>
2018-06-25 20:51:38 +0100

    Added method that would have solved the coding test I couldn't do

--- ConsoleApp1/BinaryTree.cs
+++ ConsoleApp1/BinaryTree.cs
@@ -12,6 +12,8 @@ namespace Recursion
             Root = new Node(value);
         }
 
+        public int MaxPath { get; set; }
+
         public Node Root { get; set; }
 
         public void InsertLeft(Node node, int value)
@@ -43,7 +45,8 @@ namespace Recursion
                 node.RightChild = newChild;
             }
         }
-
+        //This iterates through the tree from root to the furthest left leaf
+        //and then from root to furthest right leaf
         public void PreOrder(Node node)
         {
             Console.WriteLine("P " + Convert.ToString(node.Value));
@@ -51,6 +54,8 @@ namespace Recursion
             if (node.RightChild != null) { PreOrder(node.RightChild); }
         }
 
+        //This starts at the furthest left leaf and traverses all the nodes to
+        //the left of the root. Then it does the same for the nodes to the right of the root.        
         public void InOrder(Node node)
         {
             if (node.LeftChild != null) { InOrder(node.LeftChild); }
@@ -58,11 +63,36 @@ namespace Recursion
             if (node.RightChild != null) { InOrder(node.RightChild); }
         }
 
+        //This starts at the furthest left leaf and ends at the furthest right
+        //but traverses the tree deepest level first
         public void PostOrder(Node node)
         {
-            if(node.LeftChild != null) { PostOrder(node.LeftChild); }
-            if(node.RightChild != null) { PostOrder(node.RightChild); }
+            if (node.LeftChild != null) { PostOrder(node.LeftChild); }
+            if (node.RightChild != null) { PostOrder(node.RightChild); }
             Console.WriteLine("O " + Convert.ToString(node.Value));
         }
+
+        public void LongestPath(int currentLongest, Node node)
+        {
+            //the longest path is the number of iterations that pass
+            //the rule is that the current node value must be higher than its parent
+            if (node.LeftChild != null && node.LeftChild.Value >= node.Value)
+            {                
+                LongestPath(currentLongest + 1 , node.LeftChild);             
+            }
+            else
+            {                
+                if (currentLongest > MaxPath){MaxPath = currentLongest;}
+            }
+            if (node.RightChild != null && node.RightChild.Value > node.Value)
+            {
+                LongestPath(currentLongest + 1, node.RightChild);                
+            }
+            else
+            {                
+                if (currentLongest > MaxPath){MaxPath = currentLongest;}
+            }
+        }
+
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,22 +13,22 @@ namespace Recursion
             myTree.InsertRight(myTree.Root, 7);            
 
             Node lNode1 = myTree.Root.LeftChild;
-            Node rNode1 = myTree.Root.RightChild;
+            Node rNode1 = myTree.Root.RightChild;                        
 
             myTree.InsertRight(lNode1,6);
-            myTree.InsertLeft(rNode1,9);
+            myTree.InsertLeft(rNode1,9);            
             myTree.InsertRight(rNode1,2);
-
-            Console.WriteLine(myTree.Root.Value);
-            Console.WriteLine(lNode1.Value);
-            Console.WriteLine(rNode1.Value);
-            Console.WriteLine(lNode1.RightChild.Value);
-            Console.WriteLine(rNode1.LeftChild.Value);
-            Console.WriteLine(rNode1.RightChild.Value);
+            myTree.InsertLeft(rNode1.LeftChild, 12);
+            myTree.InsertRight(rNode1.LeftChild, 3);
+            myTree.InsertLeft(rNode1.LeftChild.LeftChild, 14);
+            myTree.InsertRight(rNode1.LeftChild.LeftChild, 6);
 
             myTree.PreOrder(myTree.Root);
             myTree.InOrder(myTree.Root);
             myTree.PostOrder(myTree.Root);
+            myTree.MaxPath = 0;
+            myTree.LongestPath(0, myTree.Root);
+            Console.WriteLine("Longest path: {0}",myTree.MaxPath);
         }
 
         
